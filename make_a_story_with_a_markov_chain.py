50cf7853da928ca2108cc29b363b8cfffa00d767
nick <nick@nicks-MacBook-Pro.local>
2021-08-08 18:14:23 +1000

    read thru in python

--- make_a_story_with_a_markov_chain.py
+++ make_a_story_with_a_markov_chain.py
@@ -1,12 +1,45 @@
 import random
-
 import requests
+import os
 
-print('\nreading https://www.gutenberg.org/files/2701/2701-0.txt (Moby Dick)...\n')
-r = requests.get('https://www.gutenberg.org/files/2701/2701-0.txt')
-# print( r.encoding )
-r.encoding = 'utf-8'
-data = r.text
+
+def read_thru(url):
+
+    # read data from url_file if possible
+    # otherwise read from the url and write to a file for later
+    
+    # to make a file which is roughly equivalenet to a url we 
+    # take of http(s):// and then just convert all forward slashes
+    # to dashes.  this is reasonably collision avoidant
+    # the other option might be to percent encode forward slashes 
+    # but that looks too ugly
+
+
+    # url2filename
+    filename = url.split('//')[-1].replace('/','-')
+
+    if os.path.exists(filename):
+        f = open(filename,'r')
+        data = f.read()
+        f.close()
+        return data
+
+    print('\nreading', url, '...\n')
+
+    # set encoding to utf8
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    data = response.text
+    
+    f = open(filename,'w')
+    f.write(data)
+    f.close()
+
+    return data
+
+
+url = 'https://www.gutenberg.org/files/2701/2701-0.txt'
+data = read_thru(url)
 
 # # write to file
 # f = open('moby_dick.txt','w',encoding="utf-8")
@@ -19,19 +52,19 @@ data = r.text
 # f.close()
 
 
-result = []
+output = []
 data = data.split() # split at whitespace
 
-position = 24158 # random.randint(0, len(data)-3)
+position = random.randint(0, len(data)-3) # 24158 
 
-result.append(data[position])
-result.append(data[position+1])
+output.append(data[position])
+output.append(data[position+1])
 
-for num_iterations in range(20):
-
+for j in range(100):
+    # print(j)
     locations_of_matches = []
     for i,_ in enumerate(data):
-        if data[i].lower() == result[-2].lower() and data[i+1].lower() == result[-1].lower():
+        if data[i].lower() == output[-2].lower() and data[i+1].lower() == output[-1].lower():
             # print(i)
             locations_of_matches.append(i)
 
@@ -39,9 +72,13 @@ for num_iterations in range(20):
     # for l in locations_of_matches:
     #     print(data[l], data[l+1], data[l+2])
     random_match_location = random.choice(locations_of_matches)
-    result.append(data[random_match_location+2])
+    new_word = data[random_match_location+2]
+    output.append(new_word)
+    print(new_word, end=' ', flush=True)
 
 
-joined_result = ' '.join(result)
-print(joined_result)
+print()
+
+# joined_result = ' '.join(output)
+# print(joined_result)
 
