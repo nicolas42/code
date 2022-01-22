84dd079062b73013c675037781a57f448a731f31
nick <nicolas42@gmail.com>
2022-01-22 21:16:58 +1000

    render text now does newlines and wrapping

--- cpp/readme.txt
+++ cpp/readme.txt
@@ -182,29 +182,3 @@ int main () {
 
 
 
-
-
-
-// https://github.com/libsdl-org/SDL_ttf/blob/main/SDL_ttf.h
-
-/* Get the dimensions of a rendered string of text */
-extern DECLSPEC int SDLCALL TTF_SizeText(TTF_Font *font, const char *text, int *w, int *h);
-extern DECLSPEC int SDLCALL TTF_SizeUTF8(TTF_Font *font, const char *text, int *w, int *h);
-extern DECLSPEC int SDLCALL TTF_SizeUNICODE(TTF_Font *font, const Uint16 *text, int *w, int *h);
-
-/* Get the measurement string of text without rendering
-   e.g. the number of characters that can be rendered before reaching 'measure_width'
-   in:
-   measure_width - in pixels to measure this text
-   out:
-   count  - number of characters that can be rendered
-   extent - latest calculated width
-*/
-extern DECLSPEC int SDLCALL TTF_MeasureText(TTF_Font *font, const char *text, int measure_width, int *extent, int *count);
-extern DECLSPEC int SDLCALL TTF_MeasureUTF8(TTF_Font *font, const char *text, int measure_width, int *extent, int *count);
-extern DECLSPEC int SDLCALL TTF_MeasureUNICODE(TTF_Font *font, const Uint16 *text, int measure_width, int *extent, int *count);
-
-
-goal: render a file of text
-
-for that we need to render each word separately, making sure not to 
--- cpp/render_text.cpp
+++ cpp/render_text.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include "SDL.h"
 #include "SDL_ttf.h"
+#define BASIC_IMPLEMENTATION 
+#include "basic.h"
 
 
 int main(int argc, char **argv) {
@@ -19,7 +21,7 @@ int main(int argc, char **argv) {
 
     TTF_Init();
     char* font_path = (char*)"data/Sans.ttf";
-    int font_size = 24;
+    int font_size = 20;
     TTF_Font *font = TTF_OpenFont(font_path, font_size);
     if (!font) {
         SDL_Log("ERROR %s %d %s\n", __FILE__, __LINE__, SDL_GetError());
@@ -28,48 +30,48 @@ int main(int argc, char **argv) {
     }
 
 
-
-    char* lorem_ipsum = (char*)malloc(1000);
-    lorem_ipsum = (char*)"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-    char *text = lorem_ipsum;
-    int i,err,x,y;
-    int extent;
-    int measure_width = 400;
-    char line[1000];
-    SDL_Color text_color = {20,20,20,0};
-    SDL_Color background_color = {255,255,255,0};
-
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
     SDL_RenderClear(renderer);
 
-    x = 0;
-    y = 0;
-    for(i=0;i<10;++i){
-        int count = 0;
-        err = TTF_MeasureText(font, text, measure_width, &extent, &count);
-        SDL_Log("count: %d\n", count);
+    int err;
+    char *text = (char*)"Hello this is a long line now. Something that won't fit on one line perhaps.\nNow we will write many things and these things will be rendered in great and awesome detail on the screen.\nAnd oh my lord how the money rolled in. The count of montey cristo.\nWhy they oughtta file that under educational too.\n";
 
-        memset(line, 0, sizeof line);
-        memcpy(line, text, count);
-        line[count] = '\0';
-        SDL_Log("%s\n", line);
+    SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+    SDL_RenderClear(renderer);
+    SDL_Color text_color = {20,20,20,0};
+    SDL_Color background_color = {255,255,255,0};
 
-        SDL_Surface *text_surface = TTF_RenderText_Shaded(font, line, text_color, background_color); 
-        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-        SDL_Rect rect;
+    SDL_Surface *text_surface;
+    SDL_Texture *text_texture;
+    SDL_Rect rect;
+    int x = 0;
+    int y = 0;
+
+    char glyph[2] = {'\0','\0'};
+    for(int i=0; text[i]!='\0'; i+=1){
+
+        glyph[0] = text[i];
+        text_surface = TTF_RenderText_Shaded(font, glyph, text_color, background_color); 
+        text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
         rect.x = x;
         rect.y = y;
         rect.w = text_surface->w;
         rect.h = text_surface->h;
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
 
-        text += count;
-        y += text_surface->h;
+        // wrap
+        if ( glyph[0] == '\n' || rect.x + text_surface->w > window_width ) {
+            x = 0;
+            y += text_surface->h;
+            rect.x = x;
+            rect.y = y;
+        } 
+        if ( glyph[0] != '\n') {
+            SDL_RenderCopy(renderer, text_texture, NULL, &rect);
+            x += text_surface->w;
+        }
 
         SDL_FreeSurface(text_surface);
-        SDL_DestroyTexture(texture);
-
+        SDL_DestroyTexture(text_texture);
     }
 
     SDL_RenderPresent(renderer);
@@ -95,86 +97,4 @@ int main(int argc, char **argv) {
 
     return EXIT_SUCCESS;
 }
-
-
-
-// void render_text(SDL_Renderer *renderer, int x, int y, char *text,
-//         TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect) {
-//     int text_width;
-//     int text_height;
-//     SDL_Surface *surface;
-//     SDL_Color textColor = {20,20,20,0};
-//     SDL_Color backgroundColor = {255,255,255,0};
-
-//     surface=TTF_RenderText_Shaded(font, text, textColor, backgroundColor);
-
-//     *texture = SDL_CreateTextureFromSurface(renderer, surface);
-//     text_width = surface->w;
-//     text_height = surface->h;
-//     SDL_FreeSurface(surface);
-//     rect->x = x;
-//     rect->y = y;
-//     rect->w = text_width;
-//     rect->h = text_height;
-// }
-
-
-
-
-
-
-// void render_some_text(SDL_Renderer *renderer, TTF_Font *font)
-// {
-
-
-//     SDL_Rect rect;
-//     SDL_Surface *text_image;
-//     SDL_Texture *texture;
-
-//     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
-//     SDL_RenderClear(renderer);
-
-//     int length = 4;
-//     char text[4][100] = { "Hello", "world", "omg", "let's see if something else is good like this for a long line omgomgomg" };
-//     int i;
-//     for(i=0;i<length;++i) {
-//         int x = 0;
-//         int y = 0;
-//         if(i!=0) { y = rect.y+rect.h; }
-
-//         SDL_Color text_color = {20,20,20,0};
-//         SDL_Color background_color = {255,255,255,0};
-//         text_image = TTF_RenderText_Shaded(font, (char*)text[i], text_color, background_color); // TTF_RenderText_Solid doesn't smooth edges
-//         texture = SDL_CreateTextureFromSurface(renderer, text_image);
-//         rect.x = x;
-//         rect.y = y;
-//         rect.w = text_image->w;
-//         rect.h = text_image->h;
-//         SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-//         SDL_FreeSurface(text_image);
-//         SDL_DestroyTexture(texture);
-//     }
-//     SDL_RenderPresent(renderer);
-// }
-
-
-
-
-        // render wrapped text
-        // To render text we need to separate it into lines.  This is either due to a newline in the text or because of line wrapping.
-        // We can use the TTF_SizeText function to determine whether the next line of text will fit on the page.
-        // If it won't then we can determine the length of the string to render using TTF_MeasureText
-        
-        // /* Get the dimensions of a rendered string of text */
-        // extern DECLSPEC int SDLCALL TTF_SizeText(TTF_Font *font, const char *text, int *w, int *h);
-        // extern DECLSPEC int SDLCALL TTF_SizeUTF8(TTF_Font *font, const char *text, int *w, int *h);
-        // extern DECLSPEC int SDLCALL TTF_SizeUNICODE(TTF_Font *font, const Uint16 *text, int *w, int *h);
-
-        // extern DECLSPEC int SDLCALL TTF_SizeText(TTF_Font *font, const char *text, int *w, int *h);
-
-        // int w,h;
-        // int index = strchr(text,'\n');
-        // // err = TTF_SizeText(font, text, &w, &h);
-	
 
